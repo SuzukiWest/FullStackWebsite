fe547208eeed086ab95a91b810f790a8153e82e0
SuzukiWest <jordan.suzuki2021@gmail.com>
2022-04-15 21:00:48 -0600

    3.7.2

---
+++ packages/client/src/hooks/pizza/use-pizza-mutations.ts
@@ -0,0 +1,37 @@
+//imports
+import { useCallback } from 'react';
+import { useMutation } from '@apollo/client';
+
+//imports
+import { CREATE_PIZZA } from '../graphql/topping/mutations';
+import { GET_PIZZAS } from '../graphql/topping/queries/get-pizzas';
+
+interface UsePizzaMutationsOutput {
+  onCreatePizza: (selectedPizza: any) => void;
+  //onUpdatePizza: (selectedPizza: any) => void;
+  //onDeletePizza: (selectedPizza: any) => Promise<void>;
+}
+
+const usePizzaMutations = (): UsePizzaMutationsOutput => {
+  const [createPizza] = useMutation(CREATE_PIZZA, { refetchQueries: [GET_PIZZAS, 'Pizzas'] });
+
+  const onCreatePizza = useCallback(
+    (selectedPizza) => {
+      try {
+        createPizza({
+          variables: {
+            createPizzaInput: {
+              name: selectedPizza.name,
+              description: selectedPizza.description,
+              ImgSrc: selectedPizza.ImgSrc,
+              toppingIds: selectedPizza.toppingIds,
+            },
+          },
+        });
+      } catch (error) {
+        throw new Error('Create-Pizza (client) not working');
+      }
+    },
+    [createPizza]
+  );
+};
